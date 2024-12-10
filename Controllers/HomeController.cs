0f81f0f0a48cff9b6a3e8455589dca52d3157b5e
548220 <548220@student.fontys.nl>
2024-12-10 13:20:03 +0100

    Homescreen IT1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace TalkToMeMario.Controllers
             return View();
         }
 
+        public IActionResult Pizza()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
