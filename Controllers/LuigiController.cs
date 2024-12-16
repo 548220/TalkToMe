57dc9e72e18d3d6e6349f91a478b4ecb3ad59f77
548220 <548220@student.fontys.nl>
2024-12-16 10:06:07 +0100

    Luigi af + layout

--- Controllers/LuigiController.cs
+++ Controllers/LuigiController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using TalkToMeMario.Models;
 
 namespace TalkToMeMario.Controllers
 {
@@ -6,7 +8,14 @@ namespace TalkToMeMario.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<PizzaOverviewViewModel> pizzas = new List<PizzaOverviewViewModel>();
+            pizzas.Add(new PizzaOverviewViewModel() { Id = 1, Name = "Margharita", Price = 12.50m });
+            pizzas.Add(new PizzaOverviewViewModel() { Id = 2, Name = "Tonno", Price = 16.00m });
+            List<BestellingViewModel> bestellingViewModels = new List<BestellingViewModel>();
+            bestellingViewModels.Add ( new BestellingViewModel(){Id = 1, KlantNaam = "Bert", Pizzas = pizzas, Status= "klaar", SubTotaal = 12.50 });
+            bestellingViewModels.Add(new BestellingViewModel() { Id = 2, KlantNaam = "Jan", Pizzas = pizzas, Status = "bezig", SubTotaal = 35.80 });
+            return View(bestellingViewModels);
         }
+
     }
 }
